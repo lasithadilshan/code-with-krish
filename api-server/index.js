3d965f38bbfe64d9b9a19f86d6d7d5018b201f78
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2025-02-26 21:48:13 +0530

    add max number endpoint and utility function to API server

--- api-server/index.js
+++ api-server/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const { getMinNumber } = require('./util.js');
+const { getMinNumber, getMaxNumber } = require('./util.js');
 
 const app = new express();
 const port = 3000;
 
-app.get('/number/max', (req, res) => { });
+// 1. GET /number/max - Returns the maximum number from a list of numbers
+app.get('/number/max', (req, res) => { 
+
+    const num1 = parseFloat(req.query.num1);
+    const num2 = parseFloat(req.query.num2);
+
+    // const numbers = [5, 10, 3, 99, 2];
+    const result = getMaxNumber(num1, num2);
+
+    res.status(result.status).json(result.data);
+
+});
+
+
+
 app.get('/number/avg', (req, res) => { }); ///number/avg?numbers=1,4,7,44,676,......n
 app.get('/number/sort', (req, res) => { }); ///number/sort?numbers=1,4,7,44,676,......n&type (asc |dec)
 app.get('/number/count', (req, res) => { }); ///number/count?numbers=1,A,saman,Kamal,676,......n&search=saman //need to return how many occurances
--- api-server/util.js
+++ api-server/util.js
@@ -14,3 +14,21 @@ function getMinNumber(num1, num2) {
 }
 
 module.exports = { getMinNumber };
+
+
+function getMaxNumber(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        return {
+            status: 400,
+            data: {
+                error: `both parameters should be numbers`,
+            },
+        };
+    }
+    return {
+        status: 200,
+        data: { max: Math.max(num1, num2) },
+    };
+}
+
+module.exports = { getMaxNumber };
