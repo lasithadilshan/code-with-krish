91344ec4fd8c287a7c4ec45fd3331a83819d33d4
lasithadilshan <dilshantilakaratne29@gmail.com>
2025-02-28 13:16:34 +0530

    react frontend created

--- nestjs/microservises-nest/cosmos/customer-service/src/app.module.ts
+++ nestjs/microservises-nest/cosmos/customer-service/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+import { Customer } from './customers/entity/customer.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [
+    CustomersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOSTNAME || 'localhost',
+      port: 3307,
+      username: 'root',
+      password: '1234',
+      database: 'cosmos',
+      entities: [Customer],
+      synchronize: true, // dont use this in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
